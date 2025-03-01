e23586968dca012c4cbb60e6bb2c7c0143f74131
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-02-28 20:33:51 -0700

    refined frontend logic

--- Frontend/huge-port-knockers-frontend/src/components/KnockerTable.tsx
+++ Frontend/huge-port-knockers-frontend/src/components/KnockerTable.tsx
@@ -1,53 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Knocker } from '../models';
 
-const mockData: Knocker[] = [
-    {
-        ip: "192.168.1.2",
-        ports: [
-            {
-                port: 22,
-                correct: true
-            },
-            {
-                port: 80,
-                correct: false
-            }
-        ],
-        authenticated: false
-    },
-    {
-        ip: "172.1.2.1",
-        ports: [
-            {
-                port: 22,
-                correct: true
-            },
-            {
-                port: 80,
-                correct: true
-            },
-            {
-                port: 443,
-                correct: true
-            },
-            {
-                port: 8080,
-                correct: true
-            }
-        ],
-        authenticated: true
-    }
-]
-
 
 export const KnockerTable = () => {
 
-    // const [data, setData] = useState<Knocker[]>([]);
-    const [data, setData] = useState<Knocker[]>(mockData);
+    const [data, setData] = useState<Knocker[]>([]);
     useEffect(() => {
         const interval = setInterval(() => {
-            fetch('http://localhost:5000/api')
+            fetch('http://localhost:5000/data')
                 .then(response => response.json())
                 .then(data => {
                 setData(data);
@@ -55,6 +15,12 @@ export const KnockerTable = () => {
         }, 2000);
         return () => clearInterval(interval);
     }, []);
+
+    const allPortsCorrect = (ip: string) => {
+        const knocker = data.find(knocker => knocker.ip === ip);
+        if (!knocker) return false;
+        return knocker.ports.every(port => port.correct);
+    }
 
     return (
         <div style={{ height: 400, width: '100%' }}>
@@ -82,8 +48,9 @@ export const KnockerTable = () => {
                                         })}
                                     </ul>
                                 </td>
-                                    <td style={{ backgroundColor: knocker.authenticated ? 'Green' : 'Red' }}>Authenticated</td>
-                                    <></>
+                                {knocker.ports.length === 4 && allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'Green' }}>Authenticated</td>}
+                                {knocker.ports.length < 4 && allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'blue'}}>Authenticating</td>}
+                                {!allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'red'}}>Authentication Failed</td>}
                             </tr>
                         )
                     })}
