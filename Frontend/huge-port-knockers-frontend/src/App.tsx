f5b9b0ad08a9532e65b3b708dbff08bad8f40168
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-02-28 21:18:19 -0700

    updates to prettify

--- Frontend/huge-port-knockers-frontend/src/App.tsx
+++ Frontend/huge-port-knockers-frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Home from './Home'
+import About from './About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
--- Frontend/huge-port-knockers-frontend/src/components/KnockerTable.tsx
+++ Frontend/huge-port-knockers-frontend/src/components/KnockerTable.tsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react';
 import { Knocker } from '../models';
+import { CircularProgress } from '@mui/material';
 
 
 export const KnockerTable = () => {
 
     const [data, setData] = useState<Knocker[]>([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const interval = setInterval(() => {
             fetch('http://localhost:5000/data')
                 .then(response => response.json())
                 .then(data => {
                 setData(data);
+                setLoading(false);
           });
-        }, 2000);
+        }, 500);
         return () => clearInterval(interval);
     }, []);
+
+    const headers = ['IP Address', 'Ports Knocked', 'Authenticated'];
 
     const allPortsCorrect = (ip: string) => {
         const knocker = data.find(knocker => knocker.ip === ip);
@@ -24,38 +29,47 @@ export const KnockerTable = () => {
 
     return (
         <div style={{ height: 400, width: '100%' }}>
-            <table style={{ width: '100%'}}>
+            {loading && <CircularProgress/>}
+            {!loading &&
+            <table style={{ width: '100%', border: '2px solid white'}}>
                 <thead>
                     <tr>
-                        <th>IP Address</th>
-                        <th>Ports Knocked</th>
-                        <th>Authenticated</th>
+                        {headers.map((header, index) => {
+                            return (
+                                <th style={{borderRight: "2px solid white", borderBottom: '2px solid white', fontSize: 35, paddingRight: 10}} key={index}>{header}</th>
+                            )
+                        })}
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((knocker, index) => {
                         return (
                             <tr key={index}>
-                                <td>{knocker.ip}</td>
-                                <td>
+                                <td style={{fontSize:30, borderRight: "2px solid white", borderBottom: '2px solid white'}}>{knocker.ip}</td>
+                                <td style={{borderRight: "2px solid white", borderBottom: '2px solid white'}}>
                                     <ul>
                                         {knocker.ports.map((port, index) => {
                                             return (
-                                                <td key={index} style={{ backgroundColor: port.correct ? 'green' : 'red' }}>
+                                                <td key={index} style={{ fontSize: 30, backgroundColor: port.correct ? 'green' : 'red' }}>
                                                     {port.port}
                                                 </td>
                                             )
                                         })}
+                                        {knocker.ports.length < 4 && allPortsCorrect(knocker.ip) && 
+                                        <td>
+                                            <CircularProgress/> 
+                                        </td>}
                                     </ul>
                                 </td>
-                                {knocker.ports.length === 4 && allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'Green' }}>Authenticated</td>}
-                                {knocker.ports.length < 4 && allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'blue'}}>Authenticating</td>}
-                                {!allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'red'}}>Authentication Failed</td>}
+                                {knocker.ports.length === 4 && allPortsCorrect(knocker.ip) && <td style={{ borderBottom: '2px solid white', borderRight: '2px solid white', backgroundColor: 'Green', fontSize:30 }}>Authenticated</td>}
+                                {knocker.ports.length < 4 && allPortsCorrect(knocker.ip) && <td style={{ borderBottom: '2px solid white', borderRight: '2px solid white', backgroundColor: 'blue', fontSize:30}}>Authenticating</td>}
+                                {!allPortsCorrect(knocker.ip) && <td style={{ backgroundColor: 'red', borderRight: '2px solid white', borderBottom: '2px solid white', fontSize:30}}>Authentication Failed</td>}
                             </tr>
                         )
                     })}
                 </tbody>
             </table>
+}
         </div>
     )
 
