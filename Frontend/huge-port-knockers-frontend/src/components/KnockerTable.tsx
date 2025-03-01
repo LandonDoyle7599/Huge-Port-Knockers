8fe41bf7e74361ec1833c1cfa4960999605c559c
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-02-28 23:04:30 -0700

    name change

--- Frontend/huge-port-knockers-frontend/src/components/KnockerTable.tsx
+++ Frontend/huge-port-knockers-frontend/src/components/KnockerTable.tsx
@@ -19,7 +19,7 @@ export const KnockerTable = () => {
         return () => clearInterval(interval);
     }, []);
 
-    const headers = ['IP Address', 'Ports Knocked', 'Authenticated'];
+    const headers = ['IP Address', 'Ports Knocked', 'Status'];
 
     const allPortsCorrect = (ip: string) => {
         const knocker = data.find(knocker => knocker.ip === ip);
