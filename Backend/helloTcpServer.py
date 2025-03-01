51fead9b49cbfb6656fb5862999ae9eebae0a641
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-03-01 09:20:54 +0000

    changes

--- Backend/helloTcpServer.py
+++ Backend/helloTcpServer.py
@@ -3,23 +3,25 @@ from port_knock_track import PortKnock
 
 
 class HelloTcpServer():
-    def __init__(self, port_knock_track):
-        self.port_knock_track = port_krock_track
+    def __init__(self, port_knock_track, stop_event):
+        self.port_knock_track = port_knock_track
+        self.stop_event = stop_event
         self.tcpListenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serverAddr = (0.0.0.0, 8080)
+        self.serverAddr = ('127.0.0.1', 8080)
         self.tcpListenSock.bind(self.serverAddr)
             
-    def runServer
-        self.tcpListenSock.listen(1)
-        while True:
-            connection, clientAddr = self.tcpListenSock.accept()
-            # check if this client address is allowed
-            # TODO use mutex
-            if self.port_knock_track.checkIpAllowed(clientAddr)
-                # OK!
-                connection.write("Hello")
-                connection.close()
-            else
-                # How about NO
-                connection.close()
+    def runServer(self):
+        while not self.stop_event.is_set():
+            self.tcpListenSock.listen(1)
+            while True:
+                connection, clientAddr = self.tcpListenSock.accept()
+                # check if this client address is allowed
+                # TODO use mutex
+                if self.port_knock_track.checkIpAllowed(clientAddr):
+                    # OK!
+                    connection.write("Hello")
+                    connection.close()
+                else:
+                    # How about NO
+                    connection.close()
 
