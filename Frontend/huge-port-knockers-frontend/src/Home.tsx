1ac4a37e1abf97732d9fd7308ae8b2e9639fc04e
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-02-28 19:58:52 -0700

    got table added

---
+++ Frontend/huge-port-knockers-frontend/src/Home.tsx
@@ -0,0 +1,15 @@
+import { KnockerTable } from "./components/KnockerTable";
+
+
+export const Home: React.FC = () => {
+    
+
+    return (
+        <>
+        <KnockerTable/>
+        </>
+    )
+    }
+
+export default Home;
+
